8400210d53ae3911e0c499f8b52dc0f0ad42dba6
nataliojpg <natalie.molina1108@gmail.com>
2024-07-29 13:46:50 +0200

    notas3

--- api_notas3/api_nota3.py
+++ api_notas3/api_nota3.py
@@ -12,6 +12,7 @@ from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
 import pandas as pd
 from pydantic import BaseModel
 import os
+from matplotlib import font_manager
 
 app = FastAPI()
 
@@ -38,71 +39,72 @@ class CandidateRequest(BaseModel):
     name: str
     lastname: str
 
+font_path = 'Poppins-Regular.ttf'
+font_prop = font_manager.FontProperties(fname=font_path)
+
 @app.get("/")
 async def hallo():
-    return FileResponse("index2.html")
+    return '/candidate-grades3'
 
-@app.post("/candidate-grades2", response_class=HTMLResponse)
-async def main(candidate: CandidateRequest):
-    name = candidate.name
-    lastname = candidate.lastname
+@app.post("/candidate-grades3", response_class=HTMLResponse)
+async def main():
 
-    if not name or not lastname:
-        raise HTTPException(status_code=400, detail="Es necesario poner el nombre y el apellido")
-    
     conn = get_db_connection()
     cursor = conn.cursor()
 
     try:
-        #conn = get_db_connection()
-        #cursor = conn.cursor()
 
         query = """
-            SELECT c.id_candidate, c.first_name, c.last_name, ga.professionality, ga.domain, ga.resilience, 
-                   ga.social_hab, ga.leadership, ga.collaboration, ga.commitment, ga.initiative, ga.id_assessment 
-            FROM candidate c 
-            INNER JOIN grades_apt ga ON c.id_candidate = ga.id_candidate 
-            WHERE c.first_name = %s AND c.last_name = %s
+            "SELECT c.id_candidate, c.active, ga.professionality, ga.domain, ga.resilience, ga.social_hab, ga.leadership, ga.collaboration, ga.commitment, ga.initiative, ga.id_assessment
+            FROM candidate c INNER JOIN grades_apt ga ON c.id_candidate = ga.id_candidate WHERE c.active = 1
         """
-        cursor.execute(query, (name, lastname))
+        cursor.execute(query)
         results2 = cursor.fetchall()
 
         if not results2:
             raise HTTPException(status_code=404, detail="Candidato no encontrado")
 
-        df2 = pd.DataFrame(results2, columns=[
-            'id_candidate', 'first_name', 'last_name', 'professionality', 'domain', 'resilience',
-            'social_hab', 'leadership', 'collaboration', 'commitment', 'initiative', 'id_assessment'
-        ])
+        df2 = pd.DataFrame(results2)
 
-        df2_1 = df2[df2["id_assessment"] == 3]
+        df2_1 = df2[df2["id_assessment"] == 1]
 
         labels = ['professionality', 'domain', 'resilience', 'social_hab', 'leadership', 'collaboration', 'commitment', 'initiative']
+        labels_esp = ['profesionalidad', 'dominio', 'resiliencia', 'hab_sociales', 'liderazgo', 'colaboración', 'compromiso', 'iniciativa']
 
         radar_chart1 = ""
 
         # Evaluación Grupal
         if not df2_1.empty:
-            data = df2_1.iloc[0][labels].values
-            data = np.concatenate((data, [data[0]]))
+
+            mean_data = df2_1[labels].mean().values
+
+
+            mean_data = np.concatenate((mean_data, [mean_data[0]]))
+    
+
             angles = np.linspace(0, 2 * np.pi, len(labels), endpoint=False).tolist()
             angles += angles[:1]
-#
+
+
             fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
-            ax.fill(angles, data, color='red', alpha=0.25)
-            ax.plot(angles, data, color='red', linewidth=2)
+            ax.fill(angles, mean_data, color='#116249', alpha=0.25)
+            ax.plot(angles, mean_data, color='#116249', linewidth=2)
+
+    
             ax.set_yticklabels([])  
             ax.set_xticks(angles[:-1])
-            ax.set_xticklabels(labels)
+            ax.set_xticklabels(labels_esp, fontproperties=font_prop)  
             for label, angle in zip(ax.get_xticklabels(), angles):
-                x, y = label.get_position()
-                label.set_position((x, y + -0.1))
+                        x, y = label.get_position()
+                        label.set_position((x, y + -0.15))
+                        ax.set_ylim(0, 5)
+                        plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)  # Ajusta los márgenes
+
             ax.set_ylim(0, 5)
-            plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)  # Ajusta los márgenes
+            plt.show()
 
-
-            radar_chart1 = plot_to_base64(plt)
-            plt.close(fig)
+        else:
+            print("No hay datos para la Evaluación Grupal")
 
        
 

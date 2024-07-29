9a40c9eaa8f9b7fb40447447b60badbc708e7c46
nataliojpg <natalie.molina1108@gmail.com>
2024-07-29 17:21:46 +0200

    tres

--- API/api.py
+++ API/api.py
@@ -18,11 +18,12 @@ DATABASE_CONFIG = {
 def get_db_connection():
     return pymysql.connect(**DATABASE_CONFIG)
 
-
+# INICIO
 @app.get("/")
 def hola():
-    return "/status"
+    return "Apis"
 
+# DATA STATUS ACTIVOS
 @app.get("/status")
 def get_data():
     try:
@@ -51,6 +52,200 @@ def get_data():
     conn.close()
 
     return JSONResponse(content=data)
+
+
+# DATA MEDIA NOTA EVAL GRUPAL ACTIVOS
+@app.get("/notas-evalgrupal", response_class=JSONResponse)
+async def main():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = """
+            SELECT c.id_candidate, c.active, ga.professionality, ga.domain, ga.resilience, ga.social_hab, ga.leadership, ga.collaboration, ga.commitment, ga.initiative, ga.id_assessment
+            FROM candidate c INNER JOIN grades_apt ga ON c.id_candidate = ga.id_candidate WHERE c.active = 1
+        """
+        cursor.execute(query)
+        results2 = cursor.fetchall()
+
+        if not results2:
+            raise HTTPException(status_code=404, detail="No se encontraron candidatos activos")
+
+        df2 = pd.DataFrame(results2, columns=[
+            'id_candidate', 'active', 'professionality', 'domain', 'resilience',
+            'social_hab', 'leadership', 'collaboration', 'commitment', 'initiative', 'id_assessment'
+        ])
+
+        df2_1 = df2[df2["id_assessment"] == 1]
+
+        labels = ['professionality', 'domain', 'resilience', 'social_hab', 'leadership', 'collaboration', 'commitment', 'initiative']
+        labels_esp = ['profesionalidad', 'dominio', 'resiliencia', 'hab_sociales', 'liderazgo', 'colaboración', 'compromiso', 'iniciativa']
+
+        if not df2_1.empty:
+            mean_data = df2_1[labels].mean().values.tolist()
+            return JSONResponse(content={"labels": labels_esp, "values": mean_data})
+
+        raise HTTPException(status_code=404, detail="No hay datos de evaluación grupal para los candidatos activos")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
+
+# DATA MEDIA NOTA EVAL 1 ACTIVOS
+@app.get("/notas-eval1", response_class=JSONResponse)
+async def main2():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = """
+            SELECT c.id_candidate, c.active, ga.professionality, ga.domain, ga.resilience, ga.social_hab, ga.leadership, ga.collaboration, ga.commitment, ga.initiative, ga.id_assessment
+            FROM candidate c INNER JOIN grades_apt ga ON c.id_candidate = ga.id_candidate WHERE c.active = 1
+        """
+        cursor.execute(query)
+        results2 = cursor.fetchall()
+
+        if not results2:
+            raise HTTPException(status_code=404, detail="No se encontraron candidatos activos")
+
+        df2 = pd.DataFrame(results2, columns=[
+            'id_candidate', 'active', 'professionality', 'domain', 'resilience',
+            'social_hab', 'leadership', 'collaboration', 'commitment', 'initiative', 'id_assessment'
+        ])
+
+        df2_1 = df2[df2["id_assessment"] == 2]
+
+        labels = ['professionality', 'domain', 'resilience', 'social_hab', 'leadership', 'collaboration', 'commitment', 'initiative']
+        labels_esp = ['profesionalidad', 'dominio', 'resiliencia', 'hab_sociales', 'liderazgo', 'colaboración', 'compromiso', 'iniciativa']
+
+        if not df2_1.empty:
+            mean_data = df2_1[labels].mean().values.tolist()
+            return JSONResponse(content={"labels": labels_esp, "values": mean_data})
+
+        raise HTTPException(status_code=404, detail="No hay datos de evaluación grupal para los candidatos activos")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
+
+# DATA MEDIA NOTA EVAL 2 ACTIVOS
+@app.get("/notas-eval2", response_class=JSONResponse)
+async def main3():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = """
+            SELECT c.id_candidate, c.active, ga.professionality, ga.domain, ga.resilience, ga.social_hab, ga.leadership, ga.collaboration, ga.commitment, ga.initiative, ga.id_assessment
+            FROM candidate c INNER JOIN grades_apt ga ON c.id_candidate = ga.id_candidate WHERE c.active = 1
+        """
+        cursor.execute(query)
+        results2 = cursor.fetchall()
+
+        if not results2:
+            raise HTTPException(status_code=404, detail="No se encontraron candidatos activos")
+
+        df2 = pd.DataFrame(results2, columns=[
+            'id_candidate', 'active', 'professionality', 'domain', 'resilience',
+            'social_hab', 'leadership', 'collaboration', 'commitment', 'initiative', 'id_assessment'
+        ])
+
+        df2_1 = df2[df2["id_assessment"] == 3]
+
+        labels = ['professionality', 'domain', 'resilience', 'social_hab', 'leadership', 'collaboration', 'commitment', 'initiative']
+        labels_esp = ['profesionalidad', 'dominio', 'resiliencia', 'hab_sociales', 'liderazgo', 'colaboración', 'compromiso', 'iniciativa']
+
+        if not df2_1.empty:
+            mean_data = df2_1[labels].mean().values.tolist()
+            return JSONResponse(content={"labels": labels_esp, "values": mean_data})
+
+        raise HTTPException(status_code=404, detail="No hay datos de evaluación grupal para los candidatos activos")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
+
+# DATA GENEROS
+@app.get("/generos", response_class=JSONResponse)
+async def generos():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = """
+            SELECT c.gender
+            FROM candidate c WHERE c.active = 1
+        """
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        if not results:
+            raise HTTPException(status_code=404, detail="No se encontraron candidatos activos")
+
+        df = pd.DataFrame(results, columns=['gender'])
+        gender_counts = df['gender'].value_counts().to_dict()
+
+        return JSONResponse(content={"labels": list(gender_counts.keys()), "values": list(gender_counts.values())})
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
+
+
+# DATA REGISTROS X MES
+@app.get("/registros", response_class=JSONResponse)
+async def registros():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = """
+            SELECT registration_date
+            FROM candidate
+        """
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        if not results:
+            raise HTTPException(status_code=404, detail="No se encontraron registros")
+
+        # Crear un DataFrame y extraer el mes
+        df = pd.DataFrame(results, columns=['registration_date'])
+        df['registration_date'] = pd.to_datetime(df['registration_date'])
+        df['month'] = df['registration_date'].dt.strftime('%Y-%m')
+
+        month_counts = df['month'].value_counts().sort_index().to_dict()
+
+        return JSONResponse(content={"labels": list(month_counts.keys()), "values": list(month_counts.values())})
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

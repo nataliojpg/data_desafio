d8694ccd69f306bd969d6bcdfd9d0a2253d691e2
Natalie Molina <167324336+nataliojpg@users.noreply.github.com>
2024-07-30 14:39:53 +0200

    Update api_grades_filter.py

--- FILTER/api_grades_filter.py
+++ FILTER/api_grades_filter.py
@@ -125,6 +125,3 @@ async def get_candidate(email: str) -> Dict[str, Any]:
     finally:
         if connection:
             connection.close()
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
